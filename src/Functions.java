c12cba9a26877a285c070180c3a048822fde01fa
Ashley Narita <kenzonarita@gmail.com>
2012-04-09 06:46:00 +1000

    need sleep

--- src/Functions.java
+++ src/Functions.java
@@ -3,75 +3,104 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Functions {
-	
-	public static void main(String args[]) throws Exception{
-		findPath("Alamein","Flinders Street");
-		
+
+    static ArrayList<Integer> checked = new ArrayList<Integer>();
+    static Stack<Integer> st = new Stack<Integer>();
+
+    public static void main(String args[]) throws Exception {
+	findPath("Belgrave", "East Camberwell");
+
+    }
+
+    public static void findPath(String startingID, String endingID) throws Exception {
+
+	Class.forName("org.sqlite.JDBC");
+	Connection conn = DriverManager.getConnection("jdbc:sqlite:ptv.db");
+	int departing, destination;
+	boolean reached = false;
+	ResultSet rs;
+
+	try {
+	    Class.forName("org.sqlite.JDBC");
+	} catch (ClassNotFoundException e) {
+	    System.out.println("missing SQLite JDBC driver: run with -cp .:sqlitejdbc-vXXX.jar");
+	    System.out.println("latest jar file available at http://www.zentus.com/sqlitejdbc/");
+	    System.exit(0);
 	}
 
-	public static void findPath(String startingID, String endingID) throws Exception{
-		
-		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection("jdbc:sqlite:ptv.db");
-		int departing, destination;
-		boolean reached = false;
-		ResultSet curr;
-		
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			System.out
-					.println("missing SQLite JDBC driver: run with -cp .:sqlitejdbc-vXXX.jar");
-			System.out
-					.println("latest jar file available at http://www.zentus.com/sqlitejdbc/");
-			System.exit(0);
+	// open database
+	try {
+	    conn = DriverManager.getConnection("jdbc:sqlite:ptv.db");
+	    Statement stat = conn.createStatement();
+	    rs = stat.executeQuery("select id from stations where name = '" + startingID + "';");
+	    departing = rs.getInt("id");
+	    rs = stat.executeQuery("select id from stations where name = '" + endingID + "';");
+	    destination = rs.getInt("id");
+	    rs.close();
+	    stat.close();
+
+	  
+	    boolean possible = recursiveFunction(departing, destination, conn);
+
+	    if (possible == true) {
+		while(st.size()!=0) {
+		    System.out.println(st.pop());
+		}
+	    }
+	} catch (SQLException e) {
+	    System.out.println("couldn't create/access database: ptv.db");
+	    System.out.println(e.getMessage());
+	    e.printStackTrace(System.out);
+	    System.exit(0);
+	}
+    }
+
+    public static boolean recursiveFunction(int station, int destination, Connection conn) throws SQLException {
+	ArrayList<Integer> connected = new ArrayList<Integer>();
+	Statement stat = conn.createStatement();
+	int j = 0;
+	if (station == destination) {
+	    st.push(station);
+
+	    return true;
+	} else {
+	    ResultSet temp;
+
+	    temp = stat.executeQuery("select distinct destination from connections where source = '" + station + "';");
+
+	    while (temp.next()) {
+		connected.add(temp.getInt("destination"));	//copy connecting stations into an arraylist
+	    }
+	    stat.close();	//close statement to avoid multiple statements being open when function recurses
+
+	    while (j < connected.size()) {	
+		checked.add(connected.get(j));
+		boolean visited = false;
+
+		for (int i = 0; i < checked.size() - 1; i++) {		//check to see if station has been visited before
+		    if (checked.get(i) == connected.get(j)) {
+			visited = true;
+
+		    }
 		}
 
-		// open database
-		try {
-			conn = DriverManager.getConnection("jdbc:sqlite:ptv.db");
-			Statement stat = conn.createStatement();
-			ResultSet rs = stat.executeQuery("select id from stations where name = '" + startingID + "';");
-			departing = rs.getInt("id");
-			System.out.print(departing);
-			rs = stat.executeQuery("select id from stations where name = '" + endingID + "';");
-			destination = rs.getInt("id");
-			System.out.print(destination);
-			rs.close();
-			
-			while(reached==false){
-			rs = stat.executeQuery("select distinct destination from connections where source = '" + departing + "';");
-			while(rs.next()){
-				
-				int current = rs.getInt("destination");
-				if(current==destination){
-					reached = true;
-					System.out.println("hooray");
-				}
-			}
-			}
-		} catch (SQLException e) {
-			System.out.println("couldn't create/access database: ptv.db");
-			System.out.println(e.getMessage());
-			e.printStackTrace(System.out);
-			System.exit(0);
+		if (visited == false) {		
+		    boolean path = recursiveFunction(connected.get(j), destination, conn); //run function again on next node
+		    if (path == true) {		//if a path is found push station number onto stack
+			st.push(station);
+			return true;
+		    }
+
 		}
+		j++;
+	    }
+
 	}
-	
-	
-	public boolean recursiveFunction(int station, int destination){
-		
-		
-		
-		
-		
-		
-		
-		
-		return false;
-		
-		
-	}
+
+	return false;
+    }
 }
